58cf255c3bd6ceda1741596445c6f13d3f3471bc
winterrdog <87463258+winterrdog@users.noreply.github.com>
2021-08-03 17:53:01 +0300

    Update shell_sort.py

--- algorithms/sort/shell_sort.py
+++ algorithms/sort/shell_sort.py
@@ -1,21 +1,34 @@
 def shell_sort(arr):
-    ''' Shell Sort
-        Complexity: O(n^2)
     '''
-    n = len(arr)
-    # Initialize size of the gap
-    gap = n//2
-    
-    while gap > 0:
-        y_index = gap
-        while y_index < len(arr):
-            y = arr[y_index]
-            x_index = y_index - gap
-            while x_index >= 0 and y < arr[x_index]:
-                arr[x_index + gap] = arr[x_index]
-                x_index = x_index - gap
-            arr[x_index + gap] = y
-            y_index = y_index + 1
-        gap = gap//2
-        
+    Shell Sort
+    Complexity: O(n^(3/2))
+    '''
+    arr_len = len(arr)
+
+    def swap(i, j):
+        arr[i], arr[j] = (arr[j], arr[i])
+
+    # Calculate interval
+    interval = 1
+    while interval < arr_len // 3:
+        interval = interval * 3 + 1
+
+    while interval >= 1:
+        for outer in range(interval, arr_len):
+            # Select the value to be inserted
+            inner = outer
+
+            # Shift element towards right and moving down left the array, "interval"
+            # times the array.
+            while inner > (interval - 1) and arr[inner - interval] >= arr[inner]:
+                # (inner - interval) means the preceding value, "interval" times back(
+                # just like saying '2 times back')
+                swap(inner, (inner - interval))
+
+                # Move down the sublist interval times.
+                inner -= interval
+
+        # Calculate interval
+        interval = (interval - 1) // 3
+
     return arr
